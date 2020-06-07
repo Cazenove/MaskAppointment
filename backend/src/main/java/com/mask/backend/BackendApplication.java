312e4edd089105c19d77d6efa2275fc46966bb28
ZYF <m13110683916@163.com>
2020-06-07 17:35:31 +0800

    添加swagger

--- backend/src/main/java/com/mask/backend/BackendApplication.java
+++ backend/src/main/java/com/mask/backend/BackendApplication.java
@@ -1,9 +1,13 @@
 package com.mask.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.mask.backend.mapper")
+@EnableTransactionManagement
 public class BackendApplication {
 
     public static void main(String[] args) {
