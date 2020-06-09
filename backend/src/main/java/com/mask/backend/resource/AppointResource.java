fb1d98a36b56d1bb88875f9b054ed96c3eb286b8
ZYF <m13110683916@163.com>
2020-06-09 17:03:12 +0800

    完善参数校验错误信息的显示

--- backend/src/main/java/com/mask/backend/resource/AppointResource.java
+++ backend/src/main/java/com/mask/backend/resource/AppointResource.java
@@ -11,13 +11,13 @@ import javax.validation.constraints.NotNull;
 @Data
 public class AppointResource {
     @NotNull
-    @Length(min = 1, max = 18)
+    @Length(min = 1, max = 18, message = "身份证号长度范围在1~18个字符")
     private String idNum;
     @NotNull
-    @Length(min = 1, max = 20)
+    @Length(min = 1, max = 20, message = "姓名长度范围在1~20个字符")
     private String namee;
     @NotNull
-    @Length(min = 1, max = 11)
+    @Length(min = 1, max = 11, message = "电话号码长度范围在1~11个字符")
     private String telephone;
     private Integer appointNum;
     private Integer placeId;
--- backend/src/main/java/com/mask/backend/resource/QueryResource.java
+++ backend/src/main/java/com/mask/backend/resource/QueryResource.java
@@ -1,8 +1,13 @@
 package com.mask.backend.resource;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 public class QueryResource {
+    @NotNull
+    @Length(min = 1, max = 11, message = "电话号码长度范围在1~11个字符")
     private String telephone;
 }
